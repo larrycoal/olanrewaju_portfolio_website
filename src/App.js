6d89f526b7a2a74090eab9ef1535068426e1ee32
Olayiwola Olanrewaju <larry_coal@outlook.com>
2022-05-09 21:32:02 -0400

    testimonials

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./app.css";
 import Skills from "./Components/Skills/skills";
 import Project from "./Components/Projects/project";
 import Header from "./Components/Header/header";
+import Testimonials from "./Components/Testimonials/testimonials";
 const App = () => {
   return (
     <section className="main-wrapper">
       <Header />
       <LandingPage />
       <Skills />
+      <Testimonials />
       <Project />
     </section>
   );
