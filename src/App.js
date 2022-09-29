532e3daf2f889d87ebd4b662310fdc8a74a2b2bf
Olayiwola Olanrewaju <larry_coal@outlook.com>
2022-09-29 11:20:00 -0400

    styling changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import LandingPage from "./Components/Landingpage/landingpage";
-import "./app.css";
+
 import Skills from "./Components/Skills/skills";
 import Project from "./Components/Projects/project";
 import Header from "./Components/Header/header";
 import Testimonials from "./Components/Testimonials/testimonials";
 import Footer from "./Components/Footer/footer";
+
+import "./app.css";
 const App = () => {
   return (
     <section className="main-wrapper">
