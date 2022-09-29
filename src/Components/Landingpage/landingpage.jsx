de814b716d1b6ef875468cf0d4d156454aeab1ac
Olayiwola Olanrewaju <larry_coal@outlook.com>
2022-09-29 11:17:11 -0400

    styling changes

--- src/Components/Landingpage/landingpage.jsx
+++ src/Components/Landingpage/landingpage.jsx
@@ -7,37 +7,33 @@ const LandingPage = () => {
     <section className="landing-page">
       <div className="hero">
         <div className="right">
-            <img src={larryEmoji} alt="herosvg" />
-
+          <img src={larryEmoji} alt="herosvg" />
         </div>
 
-          <div className="left">
-            <p className="intro">
-              Hi. I’m <span>Olayiwola Olanrewaju</span>
-            </p>
+        <div className="left">
+          <p className="intro">
+            Hi. I’m <span>Olayiwola Olanrewaju</span>
+          </p>
 
-            <h2>
-              A FULL-STACK WEB DEVELOPER WITH AN EYE FOR INTUITIVE DESIGN SKILLS
-            </h2>
-            <p>
-              I am a full stack developer, cinematographer, gamer and a UI/UX
-              enthusiast. I enjoy building pixel perfect interface and adhering
-              to development best practices. Currently learning to work with
-              ThreeJs, WebGL and Solidity
-            </p>
-            <div className="socials">
-              <p className="connect">Connect with me on:</p>
-              <a href="/">
-                <i class="fa-brands fa-github"></i>
-              </a>
-              <a href="/">
-                <i class="fa-brands fa-twitter"></i>
-              </a>
-              <a href="/">
-                <i class="fa-brands fa-linkedin"></i>
-              </a>
-            </div>
+          <h2>A FULL-STACK WEB DEVELOPER WITH AN EYE FOR INTUITIVE DESIGN</h2>
+          <p>
+            I am a software engineer based in ontario. My tech stack is MERN but
+            also comfortable working with technology outside this stack.I enjoy
+            gaming, making amatuer videos/movie and binge watching series.
+          </p>
+          <div className="socials">
+            <p className="connect">Connect with me on:</p>
+            <a href="/">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="/">
+              <i class="fa-brands fa-twitter"></i>
+            </a>
+            <a href="/">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
           </div>
+        </div>
       </div>
     </section>
   );
